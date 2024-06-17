b8c2fd30272a29b4b270aedf16b750b453fad720
Raghu <admin@Admins-MacBook-Pro.local>
2024-06-17 17:22:58 +0100

    Added new cypress project folder

--- javabasicprograms/sorting/mergesort.java
+++
@@ -1,9 +0,0 @@
-package sorting;
-
-public class mergesort {
-    //Merge sort will divide the array into a single element array. 
-    public static void main(String[] args) {
-        
-    }
-    
-}
--- mochaAPIAutomation/config/common.js
+++
@@ -1,5 +0,0 @@
-import supertest from 'supertest';
-import qa from './qa';
-const request = supertest(qa.baseURL);
-
-export default request;
